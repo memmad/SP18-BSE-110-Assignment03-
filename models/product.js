f0a0d287ab7498568f679d9dbd040c18e743e2ed
memmad <72753453+memmad@users.noreply.github.com>
2020-12-30 03:02:20 +0500

    final

--- models/product.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-
-var productSchema = mongoose.Schema({
-  name: String,
-  Price: String,
-  Description: String,
-});
-const Product = mongoose.model("Product",productSchema);
-module.exports = Product;
